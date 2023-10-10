5f74df2a86ab03330488dbb417896324848de592
Başak Dönder <basakdonder01@gmail.com>
2023-10-10 18:15:43 +0300

    tailwind config update

--- blogify-frontend/tailwind.config.ts
+++ blogify-frontend/tailwind.config.ts
@@ -8,10 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        veryDarkBlue: 'hsl(243, 87%, 12%)',
+        desaturatedBlue: 'hsl(238, 22%, 44%)',
+        brightBlue: 'hsl(224, 93%, 58%)',
+        moderateCyan: 'hsl(170, 45%, 43%)',
+        lightGrayishBlue: 'hsl(240, 75%, 98%)',
+        lightGray: 'hsl(0, 0%, 75%)',
+      },
+      fontFamily: {
+        raleway: ['Raleway', 'sans-serif'],
+        openSans: ['Open Sans', 'sans-serif'],
+      },
+      fontWeight: {
+        normal: '400',
+        bold: '700',
       },
     },
   },
