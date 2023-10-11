6723640971a335c363d6353d72bcea4a0e634720
Başak Dönder <basakdonder01@gmail.com>
2023-10-11 18:02:56 +0300

    component stories added

---
+++ blogify-frontend/stories/Header.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta } from "@storybook/react";
+import Header from "@/components/Header";
+
+export default {
+  title: "Header",
+  component: Header,
+} as Meta;
+
+export const Section = () => <Header />;
---
+++ blogify-frontend/stories/MainSection1.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta } from "@storybook/react";
+import MainSection1 from "@/components/MainSection1";
+
+export default {
+  title: "MainSection1",
+  component: MainSection1,
+} as Meta;
+
+export const Section = () => <MainSection1 />;
